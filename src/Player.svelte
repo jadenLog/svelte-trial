36d2b168bb88816cdcc732f4b33cd2d9869fd6a5
jaden.2 <jaden.2@kakaoenterprise.com>
2020-12-09 22:30:50 +0900

    Add Player Component
    Player의 socre, name 을 보여주면 score를 add, subtract 하는 component 추가.

---
+++ src/Player.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let name: string;
+    export let score: number;
+
+    const addScore = () => { score += 1; };
+    const subtractScore = () => { score -= 1; };
+</script>
+
+<style>
+    div {
+        max-width: 600px;
+        margin: 0 auto;
+        border: 1px solid gray;
+    }
+    h1 {
+        font-weight: bold;
+    }
+</style>
+
+<div>
+    <h1>{name}</h1>
+    <p>{score}</p>
+    <button type="button" on:click={addScore}>+</button>
+    <button type="button" on:click={subtractScore}>-</button>
+</div>
